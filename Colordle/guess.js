2d1f4e4daf0f0d95f33f0ffedab47354a8e65e18
Tim Wagner <t.wagner@firesys.de>
2022-05-30 15:32:42 +0200

    Beautiful backgrounds

---
+++ Colordle/guess.js
@@ -0,0 +1,31 @@
+
+let r = 0;
+let g = 0;
+let b = 0;
+let rgbs = rgb(r,g,b);
+
+window.onload = function onLoad() {
+    setBackground();
+}
+
+function setBackground() {
+    r = Math.random() * 256;
+    g = Math.random() * 256;
+    b = Math.random() * 256;
+
+    rgbs = rgb(r,g,b);
+    document.body.style.backgroundColor = rgbs;
+}
+
+function wildGuess() {
+    var red = document.getElementById("input_red").value
+    var green = document.getElementById("input_green").value
+    var blue = document.getElementById("input_blue").value
+
+    if (rgb(red, green, blue) == rgbs){
+        document.getElementById("content").innerHTML = "Bingo Bongo"
+    } else {
+        document.getElementById("content").innerHTML = "Wop wop"
+    }
+
+}
