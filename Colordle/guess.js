1158a52fea04de46ab369a8fd97e3bc15d40df60
Tim Wagner <t.wagner@firesys.de>
2022-05-30 16:09:17 +0200

    rgn

--- Colordle/guess.js
+++ Colordle/guess.js
@@ -2,7 +2,7 @@
 let r = 0;
 let g = 0;
 let b = 0;
-let rgbs = rgb(r,g,b);
+let rgbs = `rgb(${r},${g},${b})`;
 
 window.onload = function onLoad() {
     document.getElementById("content").innerHTML = "onload"
@@ -18,21 +18,17 @@ function setBackground() {
     b = Math.random() * 256;
 
     document.getElementById("content").innerHTML = "rgb"
-    rgbs = rgb(r,g,b);
+    rgbs = `rgb(${r},${g},${b})`;
     document.getElementById("content").innerHTML = "background"
     document.body.style.backgroundColor = rgbs;
 }
 
 function wildGuess() {
-    document.getElementById("content").innerHTML = "yesfs"
     var red = document.getElementById("input_red").value
-    document.getElementById("content").innerHTML = "1"
     var green = document.getElementById("input_green").value
-    document.getElementById("content").innerHTML = "2"
     var blue = document.getElementById("input_blue").value
-    document.getElementById("content").innerHTML = "5"
 
-    if (rgb(red,green,blue) == rgbs){
+    if (`rgb(${red},${green},${blue})` === rgbs){
         document.getElementById("content").innerHTML = "Bingo Bongo"
     } else {
         document.getElementById("content").innerHTML = "Wop wop"
